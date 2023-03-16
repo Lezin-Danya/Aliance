2e6c34b5511c7c3da0fb57cce5eec4458ce0e4f2
lezin-danya <lezin.danil2018@yandex.ru>
2023-03-16 20:10:33 +0800

    fix: menu button, logo

--- js/main.js
+++ js/main.js
@@ -3,17 +3,35 @@ const logo = document.querySelector('.logo-svg use');
 const mMenuToggle = document.querySelector('.mobile-menu-toggle');
 const menu = document.querySelector('.mobile-menu');
 
+const lightModeOn = (e) => { // перекрашиваем лого в черный цвет
+  navbar.classList.add('navbar-light')
+  logo.href.baseVal = 'img/sprite.svg#logo'
+}
+
+const lightModeOff = (e) => {    //  перекрашиваем лого в белый цвет
+  navbar.classList.remove('navbar-light')
+  logo.href.baseVal = 'img/sprite.svg#logo-light'
+}
+
+const openMenu = (e) => {  // открытие меню
+  menu.classList.add('is-open');
+  document.body.style.overflow='hidden';
+  mMenuToggle.classList.add('close-menu');
+  lightModeOn();
+}
+
+const closeMenu = (e) => {  // закрытие меню
+  menu.classList.remove('is-open');
+  document.body.style.overflow='';
+  mMenuToggle.classList.remove('close-menu');
+  lightModeOff();
+}
+
 window.addEventListener('scroll', ()=> {
-  if (this.scrollY >1) {
-    navbar.classList.add('navbar-light')
-    logo.href.baseVal = 'img/sprite.svg#logo'
-  } else {
-    navbar.classList.remove('navbar-light')
-    logo.href.baseVal = 'img/sprite.svg#logo-light'
-  }
+  this.scrollY >1 ? lightModeOn() : lightModeOff()
 });
 
 mMenuToggle.addEventListener('click', (e) => {
   e.preventDefault();
-  menu.classList.toggle('is-open');
+  menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
